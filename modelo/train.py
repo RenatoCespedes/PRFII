ee1eeff378b7a8c9e842bdcfaaf06d721e511c34
renato <renatocespedesf@gmail.com>
2022-07-08 19:37:31 -0500

    correccion

--- modelo/train.py
+++ modelo/train.py
@@ -50,7 +50,7 @@ def train_model(model, batch_gen, num_train_steps, valid_freq, is_save=0, graph_
         # if exists check point, starts from the check point
         ckpt = tf.train.get_checkpoint_state(os.path.dirname('save/' + graph_dir_name + '/'))
         if ckpt and ckpt.model_checkpoint_path:
-            print ('from check point!!!')
+            print('from check point!!!')
             saver.restore(sess, ckpt.model_checkpoint_path)
             
         writer = tf.summary.FileWriter('./graph/'+graph_dir_name, sess.graph)
@@ -69,7 +69,7 @@ def train_model(model, batch_gen, num_train_steps, valid_freq, is_save=0, graph_
                 writer.add_summary( summary, global_step=model.global_step.eval() )
                 
             except:
-                print "Ocurrio una exception en el training"
+                print("Ocurrio una exception en el training")
                 pass
                 
             
@@ -107,21 +107,21 @@ def train_model(model, batch_gen, num_train_steps, valid_freq, is_save=0, graph_
                     else:
                         # early stopping
                         if early_stop_count == 0:
-                            print "early stopped"
+                            print("early stopped")
                             break
                              
                         test_accr = 0
                         early_stop_count = early_stop_count -1
                         
-                    print str( int(end_time - initial_time)/60 ) + " mins" + \
+                    print(str( int(end_time - initial_time)/60 ) + " mins" + \
                         " step/seen/itr: " + str( model.global_step.eval() ) + "/ " + \
                                                str( model.global_step.eval() * model.batch_size ) + "/" + \
                                                str( round( model.global_step.eval() * model.batch_size / float(len(batch_gen.train_set)), 2)  ) + \
-                        "\tdev: " + '{:.3f}'.format(dev_accr)  + "  test: " + '{:.3f}'.format(test_accr) + "  loss: " + '{:.2f}'.format(dev_ce) 
+                        "\tdev: " + '{:.3f}'.format(dev_accr)  + "  test: " + '{:.3f}'.format(test_accr) + "  loss: " + '{:.2f}'.format(dev_ce) )
                 
         writer.close()
             
-        print ('Total de pasos : {}'.format(model.global_step.eval()) )
+        print('Total de pasos : {}'.format(model.global_step.eval()) )
         
         # result logging to file
         with open('./TEST_run_result.txt', 'a') as f:
@@ -161,7 +161,7 @@ def main(data_path, batch_size, encoder_size, num_layer, hidden_dim,
     model.build_graph()
     
     valid_freq = int( len(batch_gen.train_set) * EPOCH_PER_VALID_FREQ / float(batch_size)  ) + 1
-    print "[Info] Valid Freq = " + str(valid_freq)
+    print("[Info] Valid Freq = " + str(valid_freq))
 
     train_model(model, batch_gen, num_train_steps, valid_freq, is_save, graph_dir_name)
     
