6a7c8fd0455cbba93867f0b9587c12860d52140b
renato <renatocespedesf@gmail.com>
2022-07-09 17:36:09 -0500

    test modify

--- modelo/test.py
+++ modelo/test.py
@@ -58,7 +58,7 @@ def run_test(sess, model, batch_gen, data):
      
     cont=0
     if IS_LOGGING:
-        while(os.path.exists('analysis/audio'+str(cont)+'.txt')==True and os.path.exists('analysis/audio_label'+str(cont)+'.txt')== True):
+        while(os.path.exists('../analysis/audio'+str(cont)+'.txt')==True and os.path.exists('../analysis/audio_label'+str(cont)+'.txt')== True):
             cont=cont+1
         with open( '../analysis/audio'+str(cont)+'.txt', 'w' ) as f:
             f.write( ' '.join( [str(x) for x in list_pred] ) )
